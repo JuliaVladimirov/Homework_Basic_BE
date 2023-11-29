f1e3c6f2f3d807458e1d8f4983845d5f4dd5ee81
JuliaVladimirov <148211533+JuliaVladimirov@users.noreply.github.com>
2023-11-29 17:50:59 +0100

    HW_2023.11.27

---
+++ HomeWork_2023.11.27/Java11/src/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Yulia!");
+
+        System.out.println("Hello, my name is Yulia!");
+    }
+}
