d307193a038af05769a51df6a72e8ab3ded1fbd5
JuliaVladimirov <148211533+JuliaVladimirov@users.noreply.github.com>
2024-01-14 17:00:25 +0100

    Delete Lesson22 directory

--- Lesson22/src/main/java/org/example/Constans.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Constans {
-
-    public static final Integer WIN_NUM = 5;
-    public static final Integer USER_NUM = 5;
-    public static final Integer RANGE = 50;
-
-}
